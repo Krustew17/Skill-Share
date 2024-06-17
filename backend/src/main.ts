052e8263c2e8843fcff9d4de9e1f457f6603a130
krustew17 <krasimircho555@gmail.com>
2024-06-17 18:49:38 +0300

    updated backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,11 +3,12 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import * as cookieParser from 'cookie-parser';
 import { raw } from 'body-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
+  const port = parseInt(process.env.PORT) || 3000;
   app.use(
     '/stripe/webhook/events',
     raw({ type: 'application/json' }),
@@ -22,6 +23,14 @@ async function bootstrap() {
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('SkillShare API')
+    .setDescription('SkillShare Backend API')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
